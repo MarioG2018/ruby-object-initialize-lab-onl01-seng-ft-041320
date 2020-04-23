2acf356194fef784cdf8a664daee4b236aca5fd4
Mario Garza <mario.garza02@utrgv.edu>
2020-04-23 03:30:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  
+  attr_accessor :name,:breed
+  def initialize(name,breed="Mutt")
+    @name=name
+    @breed=breed
+  end
+  
+  # def name(name)
+  #  @name
+  #end
+  
+  #def breed(breed="Mutt")
+  #  @breed
+  #end
+end
